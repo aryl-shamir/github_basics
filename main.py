2efd81ad233bf3b3a35d14b14c7c5fc3e70800c3
Bossou Chiegueu Aryl Shamir <arylshamir98@gmail.com>
2025-04-23 15:32:22 +0100

    Revert " i will be version"
    This reverts commit 62447366229bee84e7822f3ffa85386290933b77.

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ learning = input("Are learning github? ").lower()
 if learning == "yes":
     print("That's great")
     print("i'm legend")
-    print("yes i will be ")
 else:
     print("Then you are in danger")
